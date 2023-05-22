bea05de7ee22c4267bb35574ac856b2026e12c75
Legoland <99356387+Legoland4500@users.noreply.github.com>
2023-05-22 13:01:57 -0700

    egnotes2.rb

--- egnotes1.rb
+++ egnotes1.rb
@@ -1,21 +1,63 @@
-use_bpm 143
+joke = "C:/Users/davis_lepisto/Downloads/tricktreat.wav"
 
-define :modem do |a, b, c, d|
-  use_synth :pretty_bell
-  play a
-  sleep 1/2.0
-  play b
-  sleep 1/2.0
-  play c
-  sleep 1/2.0
-  play d
-  sleep 1/2.0
+use_bpm 82
+use_synth :piano
+note = [:d5,:b4,:a4,:d5,:d5,:b4,:b4]
+
+live_loop :caaspp do
+  n = 0
+  7.times do
+    play (note[n])
+    sleep 1/4.0
+    n = n + 1
+  end
+  sample joke, amp: 2, 
+  stop
 end
 
+sleep 2
+
+live_loop :intro do
+  7.times do
+    sample :bd_zum
+    sleep 1.25
+  end
+  stop
+end
+
+sleep 10
+
+
+use_bpm 143
+
 define :threes do |a, b, c|
-  play a, sustain: 0.5, amp: 2
-  play b, sustain: 0.5, amp: 2
-  play c, sustain: 0.5, amp: 2
+  play a, sustain: 0.5, amp: 3
+  play b, sustain: 0.5, amp: 3
+  play c, sustain: 0.5, amp: 3
+end
+
+live_loop :synth2 do
+  2.times do
+    2.times do
+      use_synth :kalimba
+      threes :e3, :g3, :b3
+      sleep 1.5
+      threes :e3, :g3, :b3
+      sleep 0.5
+      threes :e3, :g3, :b3
+      sleep 2.0
+    end
+    2.times do
+      use_synth :kalimba
+      threes :d3, :e3, :b3
+      sleep 1.5
+      threes :d3, :e3, :b3
+      sleep 0.5
+      threes :d3, :e3, :b3
+      sleep 2.0
+    end
+  end
+  stop
 end
 
 live_loop :drum do
@@ -43,72 +85,82 @@ live_loop :drum do
     sleep 1/2.0
   end
   sample :drum_roll, sustain: 2
+  sample joke, amp: 2
   stop
 end
 
-sleep 16
-
-live_loop :synth1 do
-  4.times do
-    3.times do
-      modem :f4, :d4, :f4, :e4
-    end
-    modem :f4, :d4, :g4, :e4
-  end
-  stop
-end
-
-live_loop :synth2 do
-  2.times do
-    2.times do
-      use_synth :kalimba
-      threes :e3, :g3, :b3
-      sleep 1.5
-      threes :e3, :g3, :b3
-      sleep 0.5
-      threes :e3, :g3, :b3
-      sleep 2.0
-    end
-    2.times do
-      use_synth :kalimba
-      threes :d3, :e3, :b3
-      sleep 1.5
-      threes :d3, :e3, :b3
-      sleep 0.5
-      threes :d3, :e3, :b3
-      sleep 2.0
+live_loop :tippy do
+  5.times do
+    sleep 6
+    8.times do
+      sleep 1/4.0
+      sample :elec_tick, amp: 5
     end
   end
   stop
 end
 
-sleep 32
-sample :drum_splash_hard
+sleep 40
 
-live_loop :maindrum do
-  11.times do
-    sample :bd_zum, amp: 2
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :drum_snare_hard
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :bd_zum, amp: 2
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :bd_zum, amp: 2
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :drum_snare_hard
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-    sample :drum_cymbal_closed, amp: 0.5
-    sleep 1/2.0
-  end
-  sample :drum_roll, sustain: 2
-  stop
-end
+use_synth :prophet
+
+play :f5
+sleep 1/2.0
+play :f5
+sleep 1/2.0
+play :a5
+sleep 1/2.0
+play :b5
+sleep 1/2.0
+#4
+play :c6
+sleep 1/2.0
+play :b5
+sleep 1/2.0
+play :a5
+sleep 1/2.0
+play :f5
+sleep 1
+#4
+play :f5
+sleep 1/2.0
+play :a5
+sleep 1/2.0
+play :b5
+sleep 1/2.0
+#3
+play :c6
+sleep 1/2.0
+play :b5
+sleep 1/2.0
+play :a5
+sleep 1/2.0
+#3 end 1
+
+play :f4
+sleep 1/2.0
+play :f4
+sleep 1/2.0
+play :a4
+sleep 1/2.0
+play :b4
+sleep 1/2.0
+#4
+play :c5
+sleep 1/2.0
+play :b4
+sleep 1/2.0
+play :a4
+sleep 1/2.0
+play :f4
+sleep 1
+#4
+play :f4
+sleep 1/2.0
+play :a4
+sleep 1/2.0
+play :b4
+sleep 1/2.0
+#3
+play :c5
+sleep 1/2.0
